2bce3216f97814481de03c2e50f2194b8d3251ee
Julien Sagot <ju.sagot@gmail.com>
2014-01-28 20:43:29 +0100

    Added HTML render engine.

---
+++ src/render_html.ml
@@ -0,0 +1,86 @@
+open AST
+open Render
+
+module Render_html : Content_render = struct
+
+  let buffer = Buffer.create 0
+
+  let add_char = Buffer.add_char buffer
+
+  let add_string = Buffer.add_string buffer
+                                     
+  let add_strings = List.iter add_string
+
+  let rec render_inlines inlines =
+    let render_inline = function
+      | Plain(str) -> add_string str
+      | InlineCode(str) -> add_string str
+      | InlineSource(str) -> add_string str
+      | InlineMath(inlines) ->
+         add_string "<span class=\"inline_math\">";
+         render_inlines inlines;
+         add_string "<span class=\"inline_math\">";
+      | Bold(inlines) ->
+         add_string "<b>";
+         render_inlines inlines;
+         add_string "</b>"
+      | Italic(inlines) ->
+         add_string "<i>";
+         render_inlines inlines;
+         add_string "</i>"
+      | Underline(inlines) ->
+         add_string "<u>";
+         render_inlines inlines;
+         add_string "</u>"
+      | Strike(inlines) ->
+         add_string "<del>";
+         render_inlines inlines;
+         add_string "</del>"
+      | Sup(c) -> add_string "<sup>"; add_char c; add_string "</sup>"
+      | Sub(c) -> add_string "<sub>"; add_char c; add_string "</sub>"
+    in List.iter render_inline inlines
+
+  and render_title lvl inlines =
+    let lvl =
+      if lvl > 6
+      then (prerr_string
+              ("HTML only supports 6 title levels, turning "
+               ^ string_of_int lvl ^ " in <h6>."); 6)
+      else lvl in
+    add_strings ["<h"; string_of_int lvl; ">\n"];
+    render_inlines inlines;
+    add_strings ["\n</h"; string_of_int lvl; ">\n"]
+
+  and render_paragraph inlines =
+    add_string "<p>\n";
+    render_inlines inlines;
+    add_string "\n</p>\n"
+
+  and render_math_block inlines =
+    add_string "<p class=\"math_block\">\n";
+    render_inlines inlines;
+    add_string "\n</p>\n"
+
+  and render_list (ord, items) =
+    add_string @@ if ord then "<ol>\n" else "<ul>\n";
+    render_items items;
+    add_string @@ if ord then "</ol>\n" else "</ul>\n"
+                                                         
+  and render_items items =
+    let aux item =
+      let Item(inlines, child) = item in
+      render_inlines inlines;
+      match child with
+      | None -> ()
+      | Some child -> render_list child in
+    List.iter
+      (fun x -> add_string "<li>\n"; aux x; add_string "\n</li>\n")
+      items
+
+  and render_code_block data =
+    add_strings ["<pre>\n<code>\n"; data; "</pre>\n</code>\n"]
+
+  and render_source_block data =
+    add_string data
+
+end
