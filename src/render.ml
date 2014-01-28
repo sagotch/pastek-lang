bb8de35fe653937b99b3e26b3649e1c0d09ff5e0
Julien Sagot <ju.sagot@gmail.com>
2014-01-28 20:41:57 +0100

    Added Render functor. Defined a Content_render signature which should be respected by render engines.

---
+++ src/render.ml
@@ -0,0 +1,31 @@
+open AST
+open Buffer
+
+module type Content_render = sig
+    (** Functions of this module are expected to write rendering in
+     *  the buffer nammed buffer *)
+    val buffer : Buffer.t
+    val render_title : int -> inline list -> unit
+    val render_paragraph : inline list -> unit
+    val render_math_block : inline list -> unit
+    val render_list : list_t -> unit
+    val render_code_block : string -> unit
+    val render_source_block : string -> unit
+  end
+  
+module Render =
+  functor (R : Content_render) ->
+struct
+  open R
+  let render_ast =
+    List.iter
+      (function
+        | Title(level, inlines) -> render_title level inlines
+        | Paragraph(inlines) -> render_paragraph inlines
+        | MathBlock(inlines) -> render_math_block inlines
+        | List(li) -> render_list li
+        | CodeBlock(data) -> render_code_block data
+        | SourceBlock(data) -> render_source_block data)
+
+  let get_buffer_content = fun () -> Buffer.contents R.buffer
+end
