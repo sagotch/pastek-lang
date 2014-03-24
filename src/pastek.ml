3df60f18054e336a6b54bfe34feed07b89a577be
Julien Sagot <ju.sagot@gmail.com>
2014-03-24 13:54:52 +0100

    Added --config option and --no-default-config. Fixed #10.

--- src/pastek.ml
+++ src/pastek.ml
@@ -1,18 +1,29 @@
 open Lexer
 open Render_html
+open Toml
 
-let version = "2014.03.22"
+let version = "2014.03.24"
 
 let full_document = ref false
+let no_default = ref false
+let config_file = ref ""
 
 let options = Arg.align [
   "--full-document", Arg.Set full_document, 
   "Generate a full standalone document.";
+
   "--translate-only", Arg.Clear full_document, 
   "Translate into target language only (default behavior).";
-  "--version", Arg.Unit
-                 (fun () -> print_endline ("pastek version: " ^ version);
-                            exit 0), 
+
+  "--no-default-config", Arg.Set no_default, 
+  "Do not use default (~/.pastek/default_config.toml) file.";
+
+  "--config", Arg.Set_string config_file, 
+  "Use provided file as configuration file.";
+
+  "--version", Arg.Unit (fun () ->
+                         print_endline ("pastek version: " ^ version);
+                         exit 0), 
   "Print pastek version and exit.";
 ]
 
@@ -21,17 +32,42 @@ let usage = "usage: pastek [options] < input > output"
 let _ = Arg.parse options (fun s -> ()) usage
 
 let _ =
-  let config, doc =
+  
+  let parsing_err lexbuf = 
+    let pos = Lexing.lexeme_start_p lexbuf in
+    let msg =
+      "Parsing: \"" ^ Lexing.lexeme lexbuf
+      ^ "\" - Line " ^ string_of_int pos.Lexing.pos_lnum
+      ^ ", column " ^ string_of_int (pos.Lexing.pos_cnum - pos.Lexing.pos_bol)
+    in
+    failwith msg
+  
+  in
+
+  (* Parse stdin *)
+  let local_config, doc =
     let lexbuf = Lexing.from_channel stdin in
     try Lexer.parse lexbuf
-    with Parser.Error ->
-      let pos = Lexing.lexeme_start_p lexbuf in
-      let msg =
-        "Parsing: \"" ^ Lexing.lexeme lexbuf
-        ^ "\" - Line " ^ string_of_int pos.pos_lnum
-        ^ ", column " ^ string_of_int (pos.pos_cnum - pos.pos_bol) in
-      failwith msg
+    with Parser.Error -> parsing_err lexbuf
   in
+
+  (* Merge differents config tables. First load default configuration file, 
+     then merge provided configuration file, and then merge local 
+     configuration. (merging will erase previous values)
+     NB: if file default_config.toml does not exists, it will produce
+         failure. *)
+  let config =
+    if !no_default
+    then Toml.create ()
+    else Toml.from_filename
+         @@ (Sys.getenv "HOME") ^ "/.pastek/default_config.toml" in
+
+  if !config_file <> ""
+  then Toml.rec_merge config
+       @@ Toml.from_filename !config_file;
+
+  Toml.rec_merge config local_config;
+
   let r =
     new render_html
         begin
